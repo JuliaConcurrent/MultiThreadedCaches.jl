ca1710a71d4cf622c21f66457f72133b79c4a8cc
Nathan Daly <nhdaly@gmail.com>
2022-02-14 10:27:02 -0500

    Finish implementing MultiThreadedCache, and add stress test.
    This cache has no locks on access, and only has contention on a cache
    miss. It only ever holds a shared lock for a constant-time duration,
    never while executing user code. A Task requesting a key that is
    already being computed on another Task will block while that computation
    is being performed. By taking advantage of the append-only properties of
    a cache, the cache can be duplicated per-Thread, to avoid locking in the
    common case.

--- src/MultiThreadedCaches.jl
+++ src/MultiThreadedCaches.jl
@@ -6,11 +6,19 @@ export MultiThreadedCache, init_cache!
 
 struct MultiThreadedCache{K,V}
     thread_caches::Vector{Dict{K,V}}
-    base_cache::Dict{K,V}
+
+    base_cache::Dict{K,V}  # Guarded by: base_cache_lock
+    base_cache_lock::ReentrantLock
+    base_cache_futures::Dict{K,Channel{V}}  # Guarded by: base_cache_lock
 
     function MultiThreadedCache{K,V}() where {K,V}
+        thread_caches = Dict{K,V}[]
+
         base_cache = Dict{K,V}()
-        return new(Dict{K,V}[], base_cache)
+        base_cache_lock = ReentrantLock()
+        base_cache_futures = Dict{K,Channel{V}}()
+
+        return new(thread_caches, base_cache, base_cache_lock, base_cache_futures)
     end
 end
 
@@ -23,6 +31,7 @@ number of threads are set *at runtime, not precompilation time*.
 function init_cache!(cache::MultiThreadedCache{K,V}) where {K,V}
     append!(empty!(cache.thread_caches),
         Dict{K,V}[Dict{K,V}() for _ in 1:Threads.nthreads()])
+    return nothing
 end
 
 # Based upon the thread-safe Global RNG implementation in the Random stdlib:
@@ -39,7 +48,52 @@ function _thread_cache(cache::MultiThreadedCache)
     end
     return cache
 end
-@noinline _thread_cache_length_assert() = @assert false "length(cache.thread_caches) < Threads.nthreads() - Must call `init!(cache)` in Module __init__()."
+@noinline _thread_cache_length_assert() = @assert false "length(cache.thread_caches) < Threads.nthreads() - Must call `init_cache!(cache)` in Module __init__()."
+
+
+const CACHE_MISS = :__MultiThreadedCaches_key_not_found__
+
+function Base.get!(func::Base.Callable, cache::MultiThreadedCache{K,V}, key) where {K,V}
+    # If the thread-local cache has the value, we can return immediately.
+    # If not, we need to check the base cache.
+    return get!(_thread_cache(cache), key) do
+        # Even though we're using Thread-local caches, we still need to lock during
+        # construction to prevent multiple tasks redundantly constructing the same object,
+        # and potential thread safety violations due to Tasks migrating threads.
+        # NOTE that we only grab the lock if the key doesn't exist, so the mutex contention
+        # is not on the critical path for most accessses. :)
+        is_first_task = false
+        local future  # used only if the base_cache doesn't have the key
+        # We lock the mutex, but for only a short, *constant time* duration, to grab the
+        # future for this key, or to create the future if it doesn't exist.
+        @lock cache.base_cache_lock begin
+            value_or_miss = get(cache.base_cache, key, CACHE_MISS)
+            if value_or_miss !== CACHE_MISS
+                return value_or_miss::V
+            end
+            future = get!(cache.base_cache_futures, key) do
+                is_first_task = true
+                Channel{V}(1)
+            end
+        end
+        if is_first_task
+            v = func()
+            # Finally, lock again for a *constant time* to insert the computed value into
+            # the shared cache, so that we can free the Channel and future gets can read
+            # from the shared base_cache.
+            @lock cache.base_cache_lock begin
+                cache.base_cache[key] = v
+            end
+            # Return v to any other Tasks that were blocking on this key.
+            put!(future, v)
+            return v
+        else
+            # Block on the future until the first task that asked for this key finishes
+            # computing a value for it.
+            return fetch(future)
+        end
+    end
+end
 
 
 end # module
--- test/MultiThreadedCaches.jl
+++ test/MultiThreadedCaches.jl
@@ -2,14 +2,69 @@ using MultiThreadedCaches
 
 using Test
 
-@testset "basics" begin
+@info "Testing with:" Threads.nthreads()
 
+@testset "basics" begin
     cache = MultiThreadedCache{Int,Int}()
     init_cache!(cache)
 
     @test length(cache.thread_caches) == Threads.nthreads()
 
+    get!(cache, 1) do
+        return 10
+    end
 
+    # The second get!() shouldn't have any effect, and the result should still be 10.
+    @test get!(cache, 1) do
+        return 100
+    end == 10
 
+    # Even on different threads, the cache should still have a value for key `1`.
+    @sync for i in 1:100
+        Threads.@spawn begin
+            @test get!(cache, 1) do
+                return 100
+            end == 10
+        end
+    end
 end
+
+# Helper function for stress-test: returns true if all elements in iterable `v` are equal.
+function all_equal(v)
+    length(v) < 2 && return true
+    e1 = first(v)
+    i = 2
+    @inbounds for i=2:length(v)
+        v[i] == e1 || return false
+    end
+    return true
+end
+
+@testset "stress-test" begin
+    cache = MultiThreadedCache{Int,Int}()
+    init_cache!(cache)
+
+    len = 100
+    outputs = [Channel{Int}(Inf) for _ in 1:len]
+
+    @sync for i in 1:100_000
+        Threads.@spawn begin
+            key = rand(1:len)
+            value = get!(cache, len) do
+                # Randomize a value for (key,value) the *first time* it's requested
+                rand(Int)
+            end
+            # Every subsequent get!() on the cache should see the exact same value,
+            # even if requested in parallel from multiple threads.
+            put!(outputs[key], value)
+        end
+    end
+
+    for ch in outputs
+        close(ch)
+        @test all_equal(collect(ch))
+    end
+end
+
+
 
